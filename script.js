da3de85bb3f36a2e0129c3219145478352ed81a5
m-o-r-v-o <8584184+m-o-r-v-o@users.noreply.github.com>
2023-07-19 15:26:17 -0500

    Working grid with static number of squares.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const DEFAULT_GRID_SIZE = 16;
 const GRID_DIMENSIONS = 500;
 const gridSpace = document.querySelector(".grid");
+const DEFAULT_COLOR = "black";
 
 function createGrid(gridSize) {
     gridSpace.style.height = GRID_DIMENSIONS + "px";
@@ -11,11 +12,22 @@ function createGrid(gridSize) {
     for (let i=0; i<gridSquareCount; i++){
         let gridDiv = document.createElement("div");
         gridDiv.className = "grid-square";
-        gridDiv.style.backgroundColor = 'blue';
         gridDiv.style.height = gridSquareDimensions + "px";
         gridDiv.style.width = gridSquareDimensions + "px";
         gridSpace.appendChild(gridDiv);
     }
+
+    let gridSquares = document.querySelectorAll('.grid-square');
+
+    gridSquares.forEach(gridSquare => {
+        gridSquare.onmouseover = function () {
+            this.style.backgroundColor = setGridSquareColor();
+        }
+    });
+}
+
+function setGridSquareColor() {
+    return "black";
 }
 
 createGrid(DEFAULT_GRID_SIZE);
