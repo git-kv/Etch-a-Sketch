911111fe329c7c51e76a52f4e9d35d2ce233e4f2
m-o-r-v-o <8584184+m-o-r-v-o@users.noreply.github.com>
2023-07-19 14:36:10 -0500

    Creates grid with solid color

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const DEFAULT_GRID_SIZE = 16;
+const GRID_DIMENSIONS = 500;
 const gridSpace = document.querySelector(".grid");
 
 function createGrid(gridSize) {
+    gridSpace.style.height = GRID_DIMENSIONS + "px";
+    gridSpace.style.width = GRID_DIMENSIONS + "px";
     console.log("Running createGrid function.");
     gridSquareCount = gridSize ** 2;
+    gridSquareDimensions = GRID_DIMENSIONS / gridSize;
     for (let i=0; i<gridSquareCount; i++){
         let gridDiv = document.createElement("div");
         gridDiv.className = "grid-square";
+        gridDiv.style.backgroundColor = 'blue';
+        gridDiv.style.height = gridSquareDimensions + "px";
+        gridDiv.style.width = gridSquareDimensions + "px";
         gridSpace.appendChild(gridDiv);
     }
 }
