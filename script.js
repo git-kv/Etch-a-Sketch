5140896bef2abeca2815f8eef3f013754ef767c3
m-o-r-v-o <8584184+m-o-r-v-o@users.noreply.github.com>
2023-07-20 11:09:25 -0500

    Added logic to change grid size

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const DEFAULT_GRID_SIZE = 16;
-const GRID_DIMENSIONS = 500;
+const GRID_DIMENSIONS = 512;
 const gridSpace = document.querySelector(".grid");
 const DEFAULT_COLOR = "black";
+let customSize = DEFAULT_GRID_SIZE;
+const smallSizeBtn = document.querySelector('.small');
+const mediumSizeBtn = document.querySelector('.medium');
+const largeSizeBtn = document.querySelector('.large');
+const gridSizeButtons = document.querySelectorAll('.size-button');
+const applyCustomButton = document.querySelector('.apply-button');
+const resetButton = document.querySelector('.reset-button');
 
 function createGrid(gridSize) {
     gridSpace.style.height = GRID_DIMENSIONS + "px";
@@ -29,5 +36,39 @@ function createGrid(gridSize) {
 function setGridSquareColor() {
     return "black";
 }
+
+function highlightSizeButton(size) {
+    if (size == 16){
+        customSize = 16;
+        smallSizeBtn.classList.add("highlight-on");
+        mediumSizeBtn.classList.remove("highlight-on");
+        largeSizeBtn.classList.remove("highlight-on");
+    }
+    else if (size == 32){
+        customSize = 32;
+        mediumSizeBtn.classList.add("highlight-on");
+        largeSizeBtn.classList.remove("highlight-on");
+        smallSizeBtn.classList.remove("highlight-on");
+    }
+    else if (size == 64) {
+        customSize = 64;
+        largeSizeBtn.classList.add("highlight-on");
+        mediumSizeBtn.classList.remove("highlight-on");
+        smallSizeBtn.classList.remove("highlight-on");
+    }
+}
+
+
+
+gridSizeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        highlightSizeButton(button.id);
+    });
+});
+
+applyCustomButton.addEventListener('click', () => {
+    gridSpace.textContent = '';
+    createGrid(customSize);
+})
 
 createGrid(DEFAULT_GRID_SIZE);
